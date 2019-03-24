dc33188bc07c2abcd273fcc7e207db82d0cf1a55
Ricky DeFazio <rickysdefazio@gmail.com>
2019-03-24 17:49:04 -0400

    Job Role Section (nearly complete)

--- js/script.js
+++ js/script.js
@@ -2,5 +2,27 @@
 JSTD - Project 3
 ***************************/
 
+/**
+ * Global Variables
+ */
+
+const $name = $('#name');
+const $otherJobRole = $('#other-title');
+const $title = $('#title');
+
 // Sets focus on first input element
-$('#name').focus();
+$name.focus();
+
+
+/**
+  Hide input element until event on 'other' triggers it to appear.
+*/
+$otherJobRole.hide();
+
+$title.on('click', function() {
+  if ( $(this).val() === 'other' ) {
+    $otherJobRole.show();
+  } else {
+    $otherJobRole.hide();
+  }
+});
