72c0f03ebfffd6a93726304d8f9d0292ac7f8d55
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-03-25 23:13:56 -0400

    Completed Register for Activities Section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ JSTD - Project 3
 /**
  * Global Variables
  */
-
 const $name = $('#name');
 const $otherJobRole = $('#other-title');
 const $title = $('#title');
@@ -13,7 +12,8 @@ const $design = $('#design');
 const $color = $('#color');
 const $colorSelection = $color.children();
 const $activities = $('.activities');
-
+const $total = $('<p>Total: </p>');
+let sum = 0;
 // Sets focus on first input element
 $name.focus();
 
@@ -23,7 +23,6 @@ $name.focus();
 /*
   Hides input element until user selects 'other' and triggers it to appear.
 */
-
 $otherJobRole.hide();
 
 $title.on('change', function() {
@@ -40,7 +39,6 @@ $title.on('change', function() {
 /*
   When user chooses a design theme, the color field and options are updated to reflect the relevant options.
 */
-
 $design.on('change', function() {
   if ( $(this).val() === 'js puns' ) {
     $color.val('cornflowerblue');
@@ -69,18 +67,26 @@ $design.on('change', function() {
 
 // Register For Activities Section
 
-
-let sum = 0;
-const total = $('<p>Total: </p>');
-$('.activities').append(total);
+/* 
+  1. Add paragraph element containing total to bottom of activity list.
+  2. For each activity registered, the total is updated to reflect the total price.
+*/
+$activities.append($total);
 
 function addToTotal(cost) {
   if ($(':checked')){
-    total.text('Total: $' + cost);
+    $total.text(`Total: $${cost}`);
   }
 };
 
+
+/* 
+  1. Event handler listens for change on activities.
+  2. When an activity is registered for, the handler checks to see if there is a time conflict with other activities. If there is, those activities are disabled and greyed out.
+  3. Depending on which activities are registered for, the total price is reflected below the activity list.
+*/
 $activities.on('change', 'input', function() {
+  // Tuesday at 9AM time frame
   if ($('.tues9:checked').length > 0) {
     $('.tues9:not(:checked').attr('disabled', true);
     $('.tues9:not(:checked').parent().css('color', 'grey');
@@ -88,6 +94,7 @@ $activities.on('change', 'input', function() {
     $('.tues9:not(:checked').attr('disabled', false);
     $('.tues9:not(:checked').parent().css('color', '#000');
   }
+  // Tuesday at 1PM time frame
   if ($('.tues1:checked').length > 0) {
     $('.tues1:not(:checked').attr('disabled', true);
     $('.tues1:not(:checked').parent().css('color', 'grey');
@@ -95,16 +102,18 @@ $activities.on('change', 'input', function() {
     $('.tues1:not(:checked').attr('disabled', false);
     $('.tues1:not(:checked').parent().css('color', '#000');
   }
+  // Adding/Subtracting from total (main conference)
   if (this.checked && $(this).parent().hasClass('main')) {
     sum += 200;
   } else if ($(':not(:checked)') && $(this).parent().hasClass('main')) {
     sum -= 200;
-  } 
+  }
+  // Adding/subtracting from total (workshops)
   if (this.checked && $(this).parent().hasClass('workshop')){
     sum += 100;
   } else if ($(':not(:checked)') && $(this).parent().hasClass('workshop')) {
     sum -= 100;
   }
+
   addToTotal(sum);
 });
-
